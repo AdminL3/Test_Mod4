cff0fafc4a8f8aabb5e28f027f28f364c3741eb0
AdminL3 <l-blu@outlook.de>
2023-03-07 16:33:00 +0100

    comment

--- src/main/java/levi/testmod/TestMod.java
+++ src/main/java/levi/testmod/TestMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//added comment
 
 public class TestMod implements ModInitializer {
 	public static final String MOD_ID = "testmod";
