c6ea1ef001de90fd815aa75cf28d5c9ac1e7b4cd
AdminL3 <l-blu@outlook.de>
2023-03-07 16:31:07 +0100

    Initial commit

---
+++ src/main/java/levi/testmod/TestMod.java
@@ -0,0 +1,21 @@
+package levi.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//comment
+
+public class TestMod implements ModInitializer {
+	public static final String MOD_ID = "testmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger("testmod");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
